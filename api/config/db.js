26790d8af76ce43a08874365cbc214165c8ca710
Taufiq Attar <112812620+ATTTAUFIQ@users.noreply.github.com>
2025-06-17 21:45:13 +0530

    Update db.js

--- api/config/db.js
+++ api/config/db.js
@@ -1,24 +1,19 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
-let connection; // cached connection
+const connection = mysql.createConnection({
+  host: 'sql12.freesqldatabase.com',
+  user: 'sql12785324',
+  password: 'tkIUpEQZvY',
+  database: 'sql12785324',
+  port: 3306
+});
 
-async function getConnection() {
-  if (connection && connection.connection._closing === false) {
-    // existing open connection, reuse it
-    return connection;
+connection.connect((err) => {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
   }
-
-  connection = await mysql.createConnection({
-    host: 'sql12.freesqldatabase.com',
-    user: 'sql12785324',
-    password: 'tkIUpEQZvY',
-    database: 'sql12785324',
-    port: 3306,
-    connectTimeout: 10000 // 10 seconds
-  });
-
   console.log('Connected to freesqldatabase.com MySQL database');
-  return connection;
-}
+});
 
-module.exports = getConnection;
+module.exports = connection;
